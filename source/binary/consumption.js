2fc29dd4fb3dce478b7e70f37b55aeb0d684cf67
rogerbf <roger@bergfalk.nu>
2017-06-15 23:10:38 +0200

    initial implementation of commander

--- source/binary/consumption.js
+++ source/binary/consumption.js
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
+const cli = require(`commander`)
+const { version } = require(`../../package`)
+const consumption = require(`consumption`)
+const Table = require(`tty-table`)
 const main = require(`../index`)
+
+cli
+  .version(version)
+
+cli
+  .command(`get`)
+  .description(`get consumption`)
+  .option(`-e, --email [email address]`, `email address used to log in to Mitt Tele2`)
+  .option(`-p, --password [password]`, `password used to log in to Mitt Tele2`)
+  .option(`-s, --subscriptions [subscriptions]`, `choose specific subscription(s)`)
+  .action((get, options) => {
+    console.log(get)
+  })
+
+cli.parse(process.argv)
+
+process.argv.length < 3 && cli.help()
--- source/index.js
+++ source/index.js
@@ -1,6 +1,3 @@
 const homedir = require(`os`).homedir()
-const consumption = require(`consumption`)
-const Table = require(`tty-table`)
-const commander = require(`commander`)
 
 module.exports = {}
