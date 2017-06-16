02d85fe40361fa5a7bb034dd5e37c8ed6be87d02
rogerbf <roger@bergfalk.nu>
2017-06-16 23:32:37 +0200

    extract relevant keys from results

--- source/binary/consumption.js
+++ source/binary/consumption.js
@@ -5,8 +5,7 @@ const consumption = require(`consumption`)
 const Table = require(`tty-table`)
 const main = require(`../index`)
 
-cli
-  .version(version)
+cli.version(version)
 
 cli
   .command(`get`)
@@ -14,8 +13,46 @@ cli
   .option(`-e, --email [email address]`, `email address used to log in to Mitt Tele2`)
   .option(`-p, --password [password]`, `password used to log in to Mitt Tele2`)
   .option(`-s, --subscriptions [subscriptions]`, `choose specific subscription(s)`)
-  .action((get, options) => {
-    console.log(get)
+  .action(async ({ email, password, subscriptions }) => {
+    try {
+      const dataBuckets = await consumption(Object.assign(
+        {},
+        { email, password },
+        subscriptions ? { subscriptions: subscriptions.split(`,`) } : []
+      ))
+
+      const buckets = dataBuckets.map(({
+        msisdn,
+        currentPriceplan: {
+          unlimitedData,
+          DisplayName
+        },
+        euBucket: {
+          consumed: euDataConsumed,
+          total: euDataTotal
+        },
+        mainBucket: {
+          consumed,
+          leftToConsume,
+          total,
+          unitString,
+          toBeRestartedAtList
+        }
+      }) => ({
+        msisdn,
+        unlimitedData,
+        DisplayName,
+        euBucket: { consumed: euDataConsumed, total: euDataTotal },
+        consumed,
+        leftToConsume,
+        total,
+        unitString,
+        toBeRestartedAtList
+      }))
+      console.log(JSON.stringify(buckets, null, 2))
+    } catch (error) {
+      console.log(error)
+    }
   })
 
 cli.parse(process.argv)
